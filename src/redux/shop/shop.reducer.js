921c512435c07f9910a5eea678564ea993006fc5
johny2433 <johny.codmw3@gmail.com>
2020-06-18 17:59:10 +0300

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

---
+++ src/redux/shop/shop.reducer.js
@@ -0,0 +1,15 @@
+import SHOP_DATA from './shop.data'
+
+const INTITIAL_STATE = {
+    collections: SHOP_DATA
+}
+
+const shopReducer = (state = INTITIAL_STATE, action) => {
+    switch (action.type) {
+        default: 
+            return state 
+    }
+
+}
+
+export default shopReducer
